30baeb2979de35b5cea43628bd2e2a213a40b345
Manny Aguilera <maguilera5@ewu.edu>
2024-01-09 22:46:46 +0000

    setup

--- manny-website/src/App.jsx
+++ manny-website/src/App.jsx
@@ -5,18 +5,6 @@ import React from 'react'
 function App() {
   return (
     <div>
-
-    <header>
-      <h1>Manny's Website</h1>
-    </header>
-
-    <main>
-      <p>Welcome to my website!</p>
-    </main>
-
-    <footer>
-      <p>Created by Manny</p>
-    </footer>
     
     </div>
   )
