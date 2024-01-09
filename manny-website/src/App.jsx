806f3ad1f991a7b254da6e4bfbfb8ed47adf326f
Manny Aguilera <maguilera5@ewu.edu>
2024-01-09 21:08:43 +0000

    Setup

---
+++ manny-website/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+    <header>
+      <h1>Manny's Website</h1>
+    </header>
+
+    <main>
+      <p>Welcome to my website!</p>
+    </main>
+
+    <footer>
+      <p>Created by Manny</p>
+    </footer>
+    
+    </div>
+  )
+}
+
+export default App
