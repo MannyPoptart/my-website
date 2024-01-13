92fcc7f6f5fe6a371b3d3de364fc67c59595ee8b
Manny Aguilera <maguilera5@ewu.edu>
2024-01-13 07:20:23 +0000

    items added

--- manny-website/src/pages/Home.jsx
+++ manny-website/src/pages/Home.jsx
@@ -13,9 +13,11 @@ function Home() {
         <Navbar />
 
         <section id='content'>
-            <p>Welcome to my website! Here is my favorite pokemon: Maushold</p>
+            <p>Welcome to my website! Here is my favorite animal the sugar glider.</p>
 
             <img src="https://media3.giphy.com/media/eTQSTvcPIRWi8YhC9C/giphy.gif?cid=6c09b952kr2ufogef2slrjgm43vj7n0cqzecx2rgbxlizei5&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s" alt="Sugar Glider" />
+
+            <p>Feel free to contact me via my contact page</p>
 
 
 
